2b499e38dde239accba2543f8e44df8255545210
Constantin <67434748+costiagur@users.noreply.github.com>
2023-10-22 18:38:04 +0300

    unique semels for each rate
    unique semels for each rate

--- semelonce.py
+++ semelonce.py
@@ -1,17 +1,58 @@
 #סמלים שמופיעים פעם אחת
+import pandas as pd
+import numpy as np
 
 def semelonce(df,xlwriter,refmonth,prevmonth):
 
-    middf = df[df["Division"] != 90]["Elem"].value_counts()
+    middf = df[(df["Division"] != 90)&(df["Refdate"]==refmonth)&(df["CurAmount"] != 0)&(df["Elemtype"] == "addition components")]
 
-    reslist = []
-    for i in range(0,len((middf).index),1):
-        if middf.values[i] == 1:
-            reslist.append(middf.index[i])
-        #
+    ranklist = middf["Rank"].unique()
+
+    newdict= dict()
+    newdict["Empid"] = []
+    newdict["mn"] = []
+    newdict["Rank"] = []
+    newdict["Elem"] = []
+    newdict["CurAmount"] = []
+
+    for eachrank in ranklist:
+            rankdf = middf[middf["Rank"] == eachrank]
+            empnum = len(rankdf["Empid"].unique())
+            elemdf = rankdf["Elem"].value_counts()      
+            
+            for eachelem in elemdf.index:
+                elemcount = elemdf[eachelem]
+
+                if elemcount/empnum < 0.95 and elemcount/empnum > 0.05:
+                    pass
+                elif elemcount/empnum <= 0.03:
+                    newdict["Empid"] = newdict["Empid"] + list(rankdf[rankdf["Elem"] == eachelem]["Empid"])
+                    newdict["mn"] = newdict["mn"] + list(rankdf[rankdf["Elem"] == eachelem]["mn"])
+                    newdict["Rank"] = newdict["Rank"] + [eachrank] * len(list(rankdf[rankdf["Elem"] == eachelem]["Empid"]))
+                    newdict["Elem"] = newdict["Elem"] + list(rankdf[rankdf["Elem"] == eachelem]["Elem"])
+                    newdict["CurAmount"] = newdict["CurAmount"] + list(rankdf[rankdf["Elem"] == eachelem]["CurAmount"])
+                    
+                
+                elif elemcount/empnum >= 0.95:
+                    seta = set(rankdf["Empid"])
+                    setb = set(rankdf[~rankdf["Elem"].isin([eachelem])]["Empid"])
+                    difset = seta.difference(setb)
+                    
+                    if len(difset) > 0:
+                        newdict["Empid"] = newdict["Empid"] + list(difset)
+                        newdict["mn"] = newdict["mn"] + [np.nan] * len(difset)
+                        newdict["Rank"] = newdict["Rank"] + [eachrank] * len(difset) 
+                        newdict["Elem"] = newdict["Elem"] + [eachelem] * len(difset)
+                        newdict["CurAmount"] = newdict["CurAmount"] + [np.nan]* len(difset)
+                    #
+                #
+            #
+            
     #
-    resdf = df[(df["Division"] != 90)&(df["Elem"].isin(reslist))]
 
+    resdf = pd.DataFrame.from_dict(newdict)
+    resdf.sort_values(by=['Elem'],inplace=True)
+            
     resdf.head(10)
 
     resdf.to_excel(xlwriter,sheet_name="semel_once",index=False)

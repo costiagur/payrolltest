4546c16e610a6659035b95fd6b87125ed9a30722
Constantin <67434748+costiagur@users.noreply.github.com>
2023-10-31 12:13:08 +0200

    check btl above maximum

---
+++ semel94010.py
@@ -0,0 +1,12 @@
+#ברוטו ביטוח לאומי מעל התקרה
+
+def semel94010(df,xlwriter,refmonth,prevmonth,level="47465"):
+
+    level = float(level)
+
+    resdf = df[(df["Division"] != 90)&(df["Elem"] =="94010")&(df["Refdate"] == refmonth)&(df["CurAmount"] > level)]
+
+    resdf.to_excel(xlwriter,sheet_name="highgrossbtl",index=False)
+    
+    return len(resdf["Empid"].unique())
+#

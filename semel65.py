1e8c06017769cc7e9b6c64f4c503cbc117a609d7
Constantin <67434748+costiagur@users.noreply.github.com>
2024-04-10 18:53:27 +0300

    Delete semel65.py

--- semel65.py
+++
@@ -1,9 +0,0 @@
-#כוננות בחוזה אישי
-
-def semel65(df,xlwriter,refmonth,prevmonth,level="0"):  
-    middf = df[(df["Refdate"] == refmonth)&(df["Rank"].isin((185,186,44,273,515,523,524,551,587,987)))&((df["Elem"] == "65")&(df["CurQuantity"] > 0))]
-    middf.to_excel(xlwriter,sheet_name="65_hozeishi",index=False)
-
-    middf.head(10)
-
-    return len(middf)
